7c369654ceb88f01a31ae9a2cd5042b9bdaab4cb
TedCCLeung <89104258+TedCCLeung@users.noreply.github.com>
2022-04-21 10:00:43 -0400

    -

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Information on the similarity of transcription factor binding sites on the database TFDB
+#'
+#' @format A data frame with 191271 rows and 7 variables:
+#' \describe{
+#'   \item{pair}{}
+#'   \item{PCC}{}
+#'   \item{PCC_normalized}{}
+#'   \item{KL}{}
+#'   \item{KL_normalized}{}
+#'   \item{gene1}{}
+#'   \item{gene2}{}
+#' }
+"TFBS_similarity"
--- R/dev.R
+++ R/dev.R
@@ -5,22 +5,17 @@
 #usethis::use_mit_license()
 
 # Data manipulation -----------------------------------
-# usethis::use_package("dendextend")
-# usethis::use_package("circlize")
 # usethis::use_package("rlang")
 
 # Bioconductor -----------------------------------
-# usethis::use_package("BiocGenerics", min_version = "0.40.0")
-# usethis::use_package("GenomeInfoDb", min_version = "1.30.0")
-# usethis::use_package("universalmotif", min_version = "1.11.1")
-# usethis::use_package("ComplexHeatmap", min_version = "2.0.0")
+# usethis::use_package("BiocGenerics", min_version = "0.40.0", type = "Suggests")
+# usethis::use_package("universalmotif", min_version = "1.11.1", type = "Suggests")
 # usethis::use_package("GenomicRanges", min_version = "1.46.1", type = "Suggests")
 # usethis::use_package("GenomicFeatures", min_version = "1.46.1", type = "Suggests")
 # usethis::use_package("AnnotationDbi", min_version = "1.40.2", type = "Suggests")
-# usethis::use_package("BSgenome.Athaliana.TAIR.TAIR9", min_version = "1.3.1000")
-# usethis::use_package("rtracklayer")
-# usethis::use_package("cowplot")
-# usethis::use_package("Biostrings")
+# usethis::use_package("BSgenome.Athaliana.TAIR.TAIR9", min_version = "1.3.1000", type = "Suggests")
+# usethis::use_package("rtracklayer", type = "Suggests")
+# usethis::use_package("Biostrings", type = "Suggests")
 
 # Tidyverse -----------------------------------
 # usethis::use_package("stringr", min_version = "1.4.0")
@@ -39,33 +34,6 @@
 #devtools::install()
 
 # For checking packages needed -----------------------------------
-
-#df_TFfamily_colors <- read.csv("/Users/TedCCLeung/Documents/Projects/Photoperiod/2_analysis/test.csv")
-#usethis::use_data(df_TFfamily_colors, overwrite = TRUE)
-
-#CISBP_df <- CISBP_motifset %>% universalmotif::to_df()
-#CISBP_families <- data.frame(
-#  motif = CISBP_df$name,
-#  family = CISBP_df$family
-#)
-#usethis::use_data(CISBP_families)
-
-#
-# seq_path <- list.files("/Users/TedCCLeung/Documents/Projects/Photoperiod/2_analysis/2_pipeline/050_motifEnrich_CISBP_test/0_input/FASTA", full.names = TRUE)
-# list_of_seqs <- lapply(seq_path, function(x){y <- Biostrings::readDNAStringSet(x); y[lengths(y) == get_mode(lengths(y))]})
-# example_promoters <- list_of_seqs
-# names(example_promoters) <- paste0("cluster", addLeadingZeros(1:14))
-# usethis::use_data(example_promoters, overwrite = TRUE)
-
-
-# rDEI_summary <- rDEI_summary[, c("geneID",
-#                                  "EQ_DEI", "LD_DEI", "SD_DEI",
-#                                  "rDEI_LDEQ", "log2_rDEI_LDEQ",
-#                                  "rDEI_SDEQ", "log2_rDEI_SDEQ",
-#                                  "rDEI_SDLD", "log2_rDEI_SDLD")]
-# usethis::use_data(rDEI_summary, overwrite = TRUE, compress = "xz")
-
-
 
 
 
